39c04e8dc036ca1d76ff44a051faa09a2a699c08
Linka <Polina153@mail.ru>
2023-12-19 14:53:26 +0400

    how much money is changed. when added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun main(args: Array<String>) {
 
     /*while (true) {
@@ -21,13 +23,47 @@ fun main(args: Array<String>) {
     }
     println("See you later, with money")*/
 
-    var i = 1
-    while (i <= 1000) {
-        println(i)
-        if (i == 5) {
-            break
+    /*    var i = 1
+        while (i <= 1000) {
+            println(i)
+            if (i == 5) {
+                break
+            }
+            i++
+        }*/
+    while (true) {
+
+        println("How much money do you have for a lunch?")
+
+        val howMuchMoney = Scanner(System.`in`).nextInt()
+
+        /*val rich = howMuchMoney > 500
+        val notRich = howMuchMoney in 300..500
+        val medium = howMuchMoney in 100..299
+        val poor = howMuchMoney in 0..99*/
+        when (howMuchMoney) {
+            in 0..99 -> println("You can buy pamen !")
+            in 100..299 -> println("You can buy a hamburger !")
+            in 300..499 -> println("You can buy a pasta ! ")
+            in 500..1_000_000 -> println("You can buy a pizza !")
+            else -> println("Go to foodbank !")
         }
-        i++
+
+        /*if (rich) {
+            println("You can buy a pizza !")
+        } else if (notRich) {
+            println("You can buy a pasta ! ")
+        } else if (medium) {
+            println("You can buy a hamburger !")
+        } else if (poor) {
+            println("You can buy pamen !")
+        } else {
+            println("Go to foodbank !")
+            break
+        }*/
+        println()
+        println()
     }
+    println("See you later!")
 }
 
